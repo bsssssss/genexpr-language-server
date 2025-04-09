d0bccb96a7f2adba19bdb23d048b95eff4ac3475
Bss <mx.bss@proton.me>
2025-04-09 17:14:59 +0200

    cleaning before implementation of node-tree-sitter + delete web-tree-sitter

--- src/server/server.ts
+++ src/server/server.ts
@@ -14,13 +14,15 @@ import Parser from 'tree-sitter';
 import logger from "../utils/logger";
 import path from "path";
 
+/////////////////////////////////////////////////////////////////////////////////
+
 logger.info(".".repeat(30) + "Starting server" + ".".repeat(30));
 
 const connection = createConnection(ProposedFeatures.all);
 const documents = new TextDocuments(TextDocument);
 
 documents.onDidOpen((event) => {
-  logger.info("Genexpr buffer : " + event.document.uri);
+  logger.info("A document was opened: " + event.document.uri);
 
   connection.sendNotification('window/showMessage', {
     type: MessageType.Info,
