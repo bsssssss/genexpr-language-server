5f38d053d5d4c7bfa45e90b8ba6b6fdcef224e62
Bss <mx.bss@proton.me>
2025-04-10 01:54:31 +0200

    add test/test.genexpr

--- out/genexpr/parser.js
+++ out/genexpr/parser.js
@@ -4,12 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
 const tree_sitter_1 = __importDefault(require("tree-sitter"));
 const parser = new tree_sitter_1.default();
 const genexprPath = path_1.default.resolve("/Users/bss/Code/git/bsssssss/tree-sitter-genexpr");
 const GenExpr = require(genexprPath);
 parser.setLanguage(GenExpr);
-const testCode = "out1 = cycle(60);";
+//const testCode = "out1 = cycle(60);";
+const testFilePath = path_1.default.join(__dirname, "../../test/test.genexpr");
+const testCode = fs_1.default.readFileSync(testFilePath).toString();
 const tree = parser.parse(testCode);
+console.log(testCode);
 console.log(tree.rootNode.toString());
+//function getSemanticTokens
 //# sourceMappingURL=parser.js.map
--- src/genexpr/parser.ts
+++ src/genexpr/parser.ts
@@ -1,5 +1,7 @@
 import path from 'path';
+import fs from 'fs';
 import Parser from 'tree-sitter';
+import { SemanticTokensBuilder } from 'vscode-languageserver/node';
 
 const parser = new Parser();
 const genexprPath = path.resolve("/Users/bss/Code/git/bsssssss/tree-sitter-genexpr");
@@ -7,7 +9,13 @@ const GenExpr: Parser.Language = require(genexprPath);
 
 parser.setLanguage(GenExpr);
 
-const testCode = "out1 = cycle(60);";
+//const testCode = "out1 = cycle(60);";
+const testFilePath = path.join(__dirname, "../../test/test.genexpr")
+const testCode = fs.readFileSync(testFilePath).toString();
 const tree = parser.parse(testCode);
 
+console.log(testCode);
+
 console.log(tree.rootNode.toString());
+
+//function getSemanticTokens
