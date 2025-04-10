d0cb3af19f702da4d19ba589786065f90677044b
Bss <mx.bss@proton.me>
2025-04-10 05:33:12 +0200

    Start implementing syntax tree iterator, only posting to console for testing

--- out/genexpr/parser.js
+++ out/genexpr/parser.js
@@ -3,9 +3,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.tokenTypesLegend = exports.TokenTypes = void 0;
+exports.getSemanticTokens = getSemanticTokens;
 const path_1 = __importDefault(require("path"));
 const fs_1 = __importDefault(require("fs"));
 const tree_sitter_1 = __importDefault(require("tree-sitter"));
+const node_1 = require("vscode-languageserver/node");
+var TokenTypes;
+(function (TokenTypes) {
+    TokenTypes[TokenTypes["Parameter"] = 0] = "Parameter";
+})(TokenTypes || (exports.TokenTypes = TokenTypes = {}));
+//export enum TokenModifiers {
+//  Declaration,
+//  Definition
+//}
+exports.tokenTypesLegend = [
+    'parameter'
+];
 const parser = new tree_sitter_1.default();
 const genexprPath = path_1.default.resolve("/Users/bss/Code/git/bsssssss/tree-sitter-genexpr");
 const GenExpr = require(genexprPath);
@@ -14,7 +28,54 @@ parser.setLanguage(GenExpr);
 const testFilePath = path_1.default.join(__dirname, "../../test/test.genexpr");
 const testCode = fs_1.default.readFileSync(testFilePath).toString();
 const tree = parser.parse(testCode);
-console.log(testCode);
-console.log(tree.rootNode.toString());
-//function getSemanticTokens
+//console.log(testCode);
+//console.log(tree.rootNode.toString());
+function getSemanticTokens(text, tree) {
+    const builder = new node_1.SemanticTokensBuilder();
+    const lines = text.split('\n');
+    function traverseNode(node) {
+        const startPos = node.startPosition;
+        const endPos = node.endPosition;
+        switch (node.type) {
+            case "function_declaration":
+                const name = node.childForFieldName('name');
+                if (name) {
+                    const nameStr = identifierToString(name);
+                    console.log(`Function name: ${nameStr}`);
+                }
+                const params = node.childrenForFieldName('parameters');
+                const paramsStr = [];
+                if (params) {
+                    for (const param of params) {
+                        const paramName = identifierToString(param);
+                        if (paramName !== ',') {
+                            console.log(`Param: ${paramName}`);
+                            paramsStr.push(paramName);
+                        }
+                    }
+                }
+                break;
+            case 'identifier':
+                const parent = node.parent;
+                console.log(parent === null || parent === void 0 ? void 0 : parent.type);
+        }
+        // Recursively iterate through all children of rootNode
+        for (const child of node.children) {
+            traverseNode(child);
+        }
+    }
+    // Start at root node
+    traverseNode(tree.rootNode);
+    // Get the matching string of an indentifier node
+    function identifierToString(node) {
+        const line = node.startPosition.row;
+        const startChar = node.startPosition.column;
+        const endChar = node.endPosition.column;
+        // Get the string matching the identifier
+        const str = lines[line].slice(startChar, endChar);
+        return str;
+    }
+    return builder.build();
+}
+getSemanticTokens(testCode, tree);
 //# sourceMappingURL=parser.js.map
--- src/genexpr/parser.ts
+++ src/genexpr/parser.ts
@@ -3,6 +3,19 @@ import fs from 'fs';
 import Parser from 'tree-sitter';
 import { SemanticTokensBuilder } from 'vscode-languageserver/node';
 
+export enum TokenTypes {
+  Parameter
+}
+
+//export enum TokenModifiers {
+//  Declaration,
+//  Definition
+//}
+
+export const tokenTypesLegend = [
+  'parameter'
+]
+
 const parser = new Parser();
 const genexprPath = path.resolve("/Users/bss/Code/git/bsssssss/tree-sitter-genexpr");
 const GenExpr: Parser.Language = require(genexprPath);
@@ -14,8 +27,63 @@ const testFilePath = path.join(__dirname, "../../test/test.genexpr")
 const testCode = fs.readFileSync(testFilePath).toString();
 const tree = parser.parse(testCode);
 
-console.log(testCode);
+//console.log(testCode);
+//console.log(tree.rootNode.toString());
 
-console.log(tree.rootNode.toString());
+export function getSemanticTokens(text: string, tree: Parser.Tree) {
+  const builder = new SemanticTokensBuilder();
+  const lines: string[] = text.split('\n');
 
-//function getSemanticTokens
+  function traverseNode(node: Parser.SyntaxNode) {
+    const startPos = node.startPosition;
+    const endPos = node.endPosition;
+
+    switch (node.type) {
+      case "function_declaration":
+        const name = node.childForFieldName('name');
+        if (name) {
+          const nameStr = identifierToString(name);
+          console.log(`Function name: ${nameStr}`);
+        }
+
+        const params = node.childrenForFieldName('parameters');
+        const paramsStr: string[] = [];
+        if (params) {
+          for (const param of params) {
+            const paramName = identifierToString(param);
+            if (paramName !== ',') {
+              console.log(`Param: ${paramName}`);
+              paramsStr.push(paramName);
+            }
+          }
+        }
+      break;
+      // TODO: identify function decl params with this 
+      //       and handle their use in the body
+      case 'identifier':
+        const parent = node.parent;
+        console.log(parent?.type);
+    }
+
+    // Recursively iterate through all children of rootNode
+    for (const child of node.children) {
+      traverseNode(child);
+    }
+  }
+  // Start at root node
+  traverseNode(tree.rootNode);
+
+  // Get the matching string of an indentifier node
+  function identifierToString(node: Parser.SyntaxNode) {
+    const line = node.startPosition.row;
+    const startChar = node.startPosition.column;
+    const endChar = node.endPosition.column;
+    // Get the string matching the identifier
+    const str = lines[line].slice(startChar, endChar);
+    return str;
+  }
+
+  return builder.build();
+}
+
+getSemanticTokens(testCode, tree);
